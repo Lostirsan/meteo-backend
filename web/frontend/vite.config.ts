767ff1353ece9196fe7550c2b8b10106290eb9a1
Lostirsan <egorochka0@gmail.com>
2026-01-27 20:48:49 +0100

    Net

--- web/frontend/vite.config.ts
+++ web/frontend/vite.config.ts
@@ -2,5 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  preview: {
+    allowedHosts: [
+      'mindful-reverence-production-2a76.up.railway.app'
+    ]
+  }
 })
